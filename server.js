7ab053dba63ad274f0d8a03bf0a8e1b5cb4363e5
Eric Wong <ericwong12@live.com>
2020-02-10 13:54:48 -0700

    Moved files around and created folders cleaned up the application folder

---
+++ server.js
@@ -0,0 +1,22 @@
+//Calls npm package
+const express = require("express");
+
+//assigns app to the express function, function tells node that we are creating an "Express" server.
+const app = express();
+
+//Creates the port where the local server is hosted.
+//it would listen to whatever "process.env.PORT" is and if nothing is found, it would jumpt to 8000
+let PORT = process.env.PORT || 8000
+
+//This is the middleware: is methods/functions/operations that are called between processing the request and sending response in the app
+//In this case, it is for POST'ing or PUT'ing information into the server, but you would have to specify where with req.body
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+
+//This listen to what the port is, as said before. The PORT can be either what heroku could spit out or set to 8000 if nothing is found.
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
+
