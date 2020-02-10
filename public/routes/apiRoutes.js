530e92b9560ee3e5086749a6a7b0eb261b170953
Eric Wong <ericwong12@live.com>
2020-02-10 14:49:46 -0700

    linked css to the html pages api routes need to still be tweeked does not currently store information.

---
+++ public/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+//Loads the data from the DB
+let noteData = require("../../db/notesData");
+
+module.exports = function(app){
+    app.get("/api/notes", function(req, res){
+        res.json(noteData);
+    });
+    app.post("/api/notes", function (req, res) {
+        noteData.push(req.body);
+        res.json(true);
+    });
+}
--- public/routes/htmlRoutes.js
+++ public/routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 module.exports = function(app){
     //Create links to the other webpages so that index.html and notes.html dont show up on the search bar. Instead * and notes will be present
-    app.get("*", function (req, res){
-        res.sendFile(path.join(__dirname,"../public-HTML/index.html"))
-    });
+
     app.get("/notes", function (req, res){
         res.sendFile(path.join(__dirname, "../public-HTML/notes.html"))
     });
-
+    app.get("*", function (req, res){
+        res.sendFile(path.join(__dirname,"../public-HTML/index.html"))
+    });
 };
