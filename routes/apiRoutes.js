acbe2fe5ef0318bd70458aa2ddeae6a7deca2085
Eric Wong <ericwong12@live.com>
2020-02-13 16:49:15 -0700

    added a return to update the page when user click on the save or delete button

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,34 @@
 const fs = require("fs");
-//Loads the data from the DB
-let dbFile = require("../db/db.json");
+const path = require("path");
+
  
 module.exports = function(app){
 
     app.get("/api/notes", function(req, res){
         //calls dbFile which is db.json
-        res.json(dbFile);
+        res.sendFile(path.join(__dirname, "../db/db.json"));
         //This only works when user visits /api/notes or else nothing would be logged
     });
 
-    app.post("/api/notes", function (req) {
+    app.post("/api/notes", function (req, res) {
         //notes that has been entered into the dom is set to newNotes
         let newNotes = req.body;
         //reads db.json sets it as a utf-8
-        fs.readFile("./db/db.json", "utf-8", (err,data) => {
-            if(err){
-                console.log(err);
-            }
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+            if(err) throw err;
             // parses data into object
             let db = JSON.parse(data);
+            db.push(newNotes)
             //pushes the data in the notes to the db array
             let id = 1;
             for(let i = 0; i < db.length; i++){
-                db[i].id = i++
+                db[i].id = id++
             }
-            db.push(newNotes);
+            
             //this is where it takes what is in db.json and writes whatever is entered into the database. Then stringifing it to work with the dom
-            fs.writeFile("./db/db.json",JSON.stringify(db),(err) => {
-                if(err){
-                    console.log(err);
-                }
+            fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify(db),(err) => {
+                if(err) throw err;
+                return res.status(200).send("Note Added");
             });
         });
     });
@@ -41,15 +39,14 @@ module.exports = function(app){
             if(err){
                 console.log(err);
             }
-            let olddb = JSON.parse(data);
-            let updateNotes = olddb.filter(x => {
+            let oldDb = JSON.parse(data);
+            let updateNotes = oldDb.filter(x => {
                 return x.id != currentNote;
             });
             //this is where it takes what is in db.json and writes whatever is entered into the database. Then stringifing it to work with the dom
             fs.writeFile("./db/db.json",JSON.stringify(updateNotes),(err) => {
-                if(err){
-                    console.log(err);
-                }
+                if(err) throw err;
+                return res.status(200).send("Note Deleted");
             });
         });
     });
