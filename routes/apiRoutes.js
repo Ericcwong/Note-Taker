d6e2d652694fcd970d935b24bcbe87f4d5529ca7
Eric Wong <ericwong12@live.com>
2020-02-13 14:02:45 -0700

    fixed a bug where saving notes would create two

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,10 @@ const fs = require("fs");
 let dbFile = require("../db/db.json");
  
 module.exports = function(app){
+
     app.get("/api/notes", function(req, res){
         //calls dbFile which is db.json
         res.json(dbFile);
-        // assigning readfile to getdata
-        // const getData = fs.readFileSync("./db/db.json");
-        // //parses the data from db.json to objects
-        // const getNotes = JSON.parse(getData);
-        // //console logs the objects
-        // console.log(getNotes);
         //This only works when user visits /api/notes or else nothing would be logged
     });
 
@@ -26,11 +21,11 @@ module.exports = function(app){
             // parses data into object
             let db = JSON.parse(data);
             //pushes the data in the notes to the db array
-            db.push(newNotes);
             let id = 1;
             for(let i = 0; i < db.length; i++){
-                db[i].id = id++
+                db[i].id = i++
             }
+            db.push(newNotes);
             //this is where it takes what is in db.json and writes whatever is entered into the database. Then stringifing it to work with the dom
             fs.writeFile("./db/db.json",JSON.stringify(db),(err) => {
                 if(err){
@@ -41,14 +36,14 @@ module.exports = function(app){
     });
 
     app.delete("/api/notes/:id", (req, res) => {
-        let id = req.params.id;
+        let currentNote = req.params.id;
         fs.readFile("./db/db.json", "utf-8", (err,data) => {
             if(err){
                 console.log(err);
             }
-            let db = JSON.parse(data);
-            let updateNotes = db.filter(x => {
-                return x.id != id;
+            let olddb = JSON.parse(data);
+            let updateNotes = olddb.filter(x => {
+                return x.id != currentNote;
             });
             //this is where it takes what is in db.json and writes whatever is entered into the database. Then stringifing it to work with the dom
             fs.writeFile("./db/db.json",JSON.stringify(updateNotes),(err) => {
