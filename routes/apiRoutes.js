624483c875ccabdda5883ad49d72d85a99735789
Eric Wong <ericwong12@live.com>
2020-02-13 17:18:00 -0700

    added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,9 +5,8 @@ const path = require("path");
 module.exports = function(app){
 
     app.get("/api/notes", function(req, res){
-        //calls dbFile which is db.json
+        //calls db file which is db.json
         res.sendFile(path.join(__dirname, "../db/db.json"));
-        //This only works when user visits /api/notes or else nothing would be logged
     });
 
     app.post("/api/notes", function (req, res) {
@@ -15,11 +14,13 @@ module.exports = function(app){
         let newNotes = req.body;
         //reads db.json sets it as a utf-8
         fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
+            //check for errors
             if(err) throw err;
             // parses data into object
             let db = JSON.parse(data);
+            //pushes to the db array
             db.push(newNotes)
-            //pushes the data in the notes to the db array
+            //assigns each note that is entered an id number
             let id = 1;
             for(let i = 0; i < db.length; i++){
                 db[i].id = id++
@@ -28,6 +29,7 @@ module.exports = function(app){
             //this is where it takes what is in db.json and writes whatever is entered into the database. Then stringifing it to work with the dom
             fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify(db),(err) => {
                 if(err) throw err;
+                //This lets the server know that a note has been added and refreshes the page
                 return res.status(200).send("Note Added");
             });
         });
@@ -35,17 +37,21 @@ module.exports = function(app){
 
     app.delete("/api/notes/:id", (req, res) => {
         let currentNote = req.params.id;
+        //reads the current json file
         fs.readFile("./db/db.json", "utf-8", (err,data) => {
             if(err){
                 console.log(err);
             }
+            //assigns the current db and parses the data
             let oldDb = JSON.parse(data);
+            //This is where the deleteing happens. updatedNotes variable is filtering the json file (array) if x id does not equal the rest, remove it from the array.
             let updateNotes = oldDb.filter(x => {
                 return x.id != currentNote;
             });
             //this is where it takes what is in db.json and writes whatever is entered into the database. Then stringifing it to work with the dom
             fs.writeFile("./db/db.json",JSON.stringify(updateNotes),(err) => {
                 if(err) throw err;
+                //This lets the server know that a note has been added and refreshes the page
                 return res.status(200).send("Note Deleted");
             });
         });
